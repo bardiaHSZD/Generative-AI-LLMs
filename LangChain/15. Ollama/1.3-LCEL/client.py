518d3a7d31f170652e514c4c6b5940fa407146d3
Bardia Hassanzadeh <bardia.hszd@gmail.com>
2026-02-12 09:25:24 -0800

    Add a translator client server Ollama app

--- LangChain/15. Ollama/1.3-LCEL/client.py
+++ LangChain/15. Ollama/1.3-LCEL/client.py
@@ -2,25 +2,23 @@ import requests
 import streamlit as st
 
 
-def get_groq_response(input_text):
+def get_ollama_response(input_text):
     json_body={
   "input": {
     "language": "French",
-    "text": "Hi"
+    "text": input_text
   },
   "config": {},
   "kwargs": {}
 }
-    response=requests.post("http://127.0.0.1:8000/chain/invoke",json_body)
-
-    print(response.json())
-
-
-    return response.json()
+    response = requests.post("http://127.0.0.1:8000/chain/invoke", json=json_body, timeout=60)
+    response.raise_for_status()
+    data = response.json()
+    return data.get("output", "")
 
 ## Streamlit app
 st.title("LLM Application Using LCEL")
 input_text=st.text_input("Enter the text you want to convert to french")
 
 if input_text:
-    st.write(get_groq_response(input_text))
+    st.write(get_ollama_response(input_text))
--- LangChain/15. Ollama/1.3-LCEL/serve.py
+++ LangChain/15. Ollama/1.3-LCEL/serve.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_groq import ChatGroq
+from langchain_ollama import ChatOllama
 import os
 from langserve import add_routes
 from dotenv import load_dotenv
 load_dotenv()
 
-groq_api_key=os.getenv("GROQ_API_KEY")
-model=ChatGroq(model="Gemma2-9b-It",groq_api_key=groq_api_key)
+model=ChatOllama(model="phi4")
 
 # 1. Create prompt template
 system_template = "Translate the following into {language}:"
@@ -39,5 +38,6 @@ add_routes(
 if __name__=="__main__":
     import uvicorn
     uvicorn.run(app,host="127.0.0.1",port=8000)
+
 
 
