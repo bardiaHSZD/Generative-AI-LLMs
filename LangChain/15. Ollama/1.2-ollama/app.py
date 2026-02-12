c0723234d8469d629b23fdf394e96612d3811c7b
Bardia Hassanzadeh <bardia.hszd@gmail.com>
2026-02-11 23:48:32 -0800

    Upload Ollama Streamlit

--- LangChain/15. Ollama/1.2-ollama/app.py
+++ LangChain/15. Ollama/1.2-ollama/app.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from langchain_community.llms import Ollama
+from langchain_ollama import ChatOllama
 import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -22,16 +22,17 @@ prompt=ChatPromptTemplate.from_messages(
 )
 
 ## streamlit framework
-st.title("Langchain Demo With Gemma Model")
+st.title("LangChain Demo with Ollama (llama2)")
 input_text=st.text_input("What question you have in mind?")
 
 
-## Ollama Llama2 model
-llm=Ollama(model="gemma:2b")
+## Ollama chat model
+llm=ChatOllama(model="llama2:latest")
 output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
 
 if input_text:
     st.write(chain.invoke({"question":input_text}))
+
 
 
