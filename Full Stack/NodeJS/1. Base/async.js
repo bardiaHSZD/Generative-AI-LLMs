7a5d113f56019008de4d89588d6b2dacccb48ac7
Bardia Hassanzadeh <bardia.hszd@gmail.com>
2026-02-26 14:50:47 -0800

    Add Async Promises

--- Full Stack/NodeJS/1. Base/async.js
+++ Full Stack/NodeJS/1. Base/async.js
@@ -1,33 +1,45 @@
-const fetchData = () => {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('Done!');
-    }, 1500);
+console.log('1. Sync start');
+
+setTimeout(() => {
+  console.log('7. Callback macrotask #1 (setTimeout 0ms)');
+  setTimeout(() => console.log('10. Nested callback macrotask #2'), 0);
+}, 0);
+
+// Callback hell example
+function callbackHell(cb) {
+  setTimeout(() => {
+    console.log('Callback Hell Step 1');
+    cb();
+  }, 0);
+}
+callbackHell(() => {
+  setTimeout(() => {
+    console.log('Callback Hell Step 2');
+  }, 0);
+});
+
+Promise.resolve()
+  .then(() => console.log('3. Promise microtask #1'));
+
+Promise.resolve()
+  .then(() => {
+    console.log('4. Promise microtask #2');
+    Promise.resolve().then(() => console.log('6. Nested Promise microtask'));
   });
-  return promise;
-};
 
-setTimeout(() => {
-  console.log('Timer is done!');
-  fetchData()
-    .then(text => {
-      console.log(text);
-      return fetchData();
-    })
-    .then(text2 => {
-      console.log(text2);
-    });
-}, 2000);
+Promise.resolve()
+  .then(() => console.log('5. Promise microtask #3'));
 
-console.log('Hello!');
-console.log('Hi!');
+setTimeout(() => console.log('8. Callback macrotask #3 (setTimeout 0ms)'), 0);
 
-setTimeout(() => {
-    console.log("Step 1");
-    setTimeout(() => {
-        console.log("Step 2");
-        setTimeout(() => {
-            console.log("Step 3");
-        }, 1000);
-    }, 1000);
-}, 1000);
+Promise.all([
+  Promise.resolve('A').then(val => console.log('2. Promise.all A: ' + val)),
+  new Promise(resolve => {
+    setTimeout(() => {  // Callback inside Promise
+      resolve('B');
+      console.log('9. Callback in Promise resolved');
+    }, 0);
+  })
+]).then(() => console.log('11. Promise.all complete (after all Promises)'));
+
+console.log('12. Sync end');
